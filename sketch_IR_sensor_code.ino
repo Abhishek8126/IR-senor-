a26f23aef64ca9788213bad4a0e8c04093a47d30
Abhishek8126 <66129306+Abhishek8126@users.noreply.github.com>
2020-06-20 17:45:18 +0530

    Update sketch_IR_sensor_code.ino

--- sketch_IR_sensor_code.ino
+++ sketch_IR_sensor_code.ino
@@ -3,15 +3,15 @@ Servo motor1;
 
 void setup()
 {
- motor1.attach(D0);
- pinMode(D1,INPUT);
+ motor1.attach(2);
+ pinMode(2,INPUT);
  Serial.begin(9600);
 }
 
 void loop() 
 {
   Serial.println("IR is");
-  Serial.println(digitalRead(D1));
+  Serial.println(digitalRead(2));
   delay(1000);
 
 }
